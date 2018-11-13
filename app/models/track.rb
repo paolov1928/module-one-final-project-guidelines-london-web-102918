50d2168c1493a01a3d5a37ed00352a0d1a5cd734
rosen777 <rosen.toshev@flatironschool.com>
2018-11-13 18:46:11 +0000

    Methods added

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,10 @@
 class Track < ActiveRecord::Base
+has_many :playlist_tracks
+has_many :playlists, through: :playlist_track
+
+def enter_song(song_title)
+  puts "We know that you are a Swiftie! Please, enter your favorite Taylor Swift song:"
+  self.title.select{|s| s.title == song_title}
+end
+
 end
---
+++ db/migrate/002_create_tracks.rb
@@ -0,0 +1,7 @@
+class CreateTracks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tracks do |t|
+      t.string :title
+  end
+end
+end
