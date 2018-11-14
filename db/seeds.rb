7f8142ab57686d2ccdc69bc17fd43d3cbcb246d3
paolov1928 <ventura.paolo@gmail.com>
2018-11-14 10:16:55 +0000

    adding in some base values

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,17 @@ playlist3 = Playlist.create(:name=>"Back Together")
 playlist_track1 = PlaylistTrack.create(playlist_id: 1, track_id: 1)
 playlist_track2 = PlaylistTrack.create(playlist_id: 2, track_id: 1)
 playlist_track3 = PlaylistTrack.create(playlist_id: 2, track_id: 3)
+
+
+#Taylor Swift GIFS
+whatever = "https://media.giphy.com/media/uPD6M9fj1elG/giphy.gif"
+lsign = "https://media.giphy.com/media/rfskmSvktqSoo/giphy.gif"
+suckit = "https://media.giphy.com/media/Lk9SktDAGzYUU/giphy.gif"
+absurd = "https://media.giphy.com/media/8QyCtDLrz4fsc/giphy.gif"
+wave = "https://media.giphy.com/media/GUlWK3Ypfah7a/giphy.gif"
+eyeroll = "https://media.giphy.com/media/CfGFJw6a73XmE/giphy.gif"
+rawr = "https://media.giphy.com/media/qvWCTAG5MxOmc/giphy.gif"
+
+#Entry questions
+
+{birthday: "December 13, 1989", birthplace: "Reading, Pennsylvania", net_worth: "$320 million", }
